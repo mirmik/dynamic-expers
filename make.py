bb01994d68eaad4fda904296390aafcec1f6c170
mirmik <mirmikns@yandex.ru>
2022-06-04 13:09:01 +0300

    'a'

--- make.py
+++ make.py
@@ -3,11 +3,12 @@
 import licant
 
 licant.include("rabbit")
+licant.include("ralgo")
 
 licant.cxx_application("app",
 	sources = ["src/main.cpp"],
-	libs = ["reactphysics3d", "GL", "GLEW", "glfw", "igris", "nos", "ralgo"],
-	mdepends = ["rabbit", "rabbit.opengl"] 
+	libs = ["reactphysics3d", "GL", "GLEW", "glfw", "igris", "nos"],
+	mdepends = ["rabbit", "rabbit.opengl", "ralgo"] 
 )
 
 licant.ex("app")
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,137 @@
 #include <reactphysics3d/reactphysics3d.h>
 #include <rabbit/opengl/qtscene.h>
 #include <rabbit/opengl/window.h>
+#include <rabbit/compat/reactphysics3d.h>
+#include <rabbit/mesh/mesh.h> 
+#include <rabbit/mesh/primitives.h> 
 
 using namespace reactphysics3d;
 
+class RigidBodyDrawer : public rabbit::drawable_object 
+{ 
+    RigidBody* m_body;
+    rabbit::mesh<float> m_mesh;
+
+public:
+    RigidBodyDrawer(RigidBody* body) : m_body(body)
+    {
+        m_mesh = rabbit::sphere_mesh(0.1, 10, 10);
+    }
+
+    RigidBodyDrawer(RigidBody* body, rabbit::mesh<float> mesh) : m_body(body), m_mesh(mesh)
+    {}
+
+    void draw_on(rabbit::view& v) override
+    {
+        v.draw_mesh(m_mesh, rabbit::react_cast(m_body->getTransform()).to_mat4());
+    }
+};
+
+class RigidBodyPair 
+{
+public:
+    RigidBody* body;
+    std::unique_ptr<RigidBodyDrawer> drawer;
+
+public:
+    RigidBodyPair() : body(nullptr) {}
+
+    RigidBodyPair(RigidBody* body) : body(body), drawer(new RigidBodyDrawer(body))
+    {}
+
+    RigidBodyPair(RigidBodyPair&& oth) : body(oth.body), drawer(std::move(oth.drawer))
+    {
+        oth.body = nullptr;
+    }
+
+    RigidBodyPair& operator=(RigidBodyPair&& oth)
+    {
+        body = oth.body;
+        drawer = std::move(oth.drawer);
+        oth.body = nullptr;
+        return *this;
+    }
+
+    static RigidBodyPair make(PhysicsWorld* world, const ralgo::pose3<float>& pose)
+    {
+        auto body = world->createRigidBody(rabbit::react_cast(pose));
+        return RigidBodyPair(body);
+    }
+
+    ralgo::pose3<float> get_pose() const
+    {
+        return rabbit::react_cast(body->getTransform());
+    }
+};
+
+HingeJoint * create_hinge_joint(
+    PhysicsWorld* world,
+    rabbit::vec3f point, 
+    rabbit::vec3f _axis,
+    RigidBody* body1,
+    RigidBody* body2)
+{
+    const Vector3 anchorPoint = rabbit::react_cast(point);
+    const Vector3 axis = rabbit::react_cast(_axis);
+    HingeJointInfo jointInfo(body1, body2, anchorPoint, axis);
+    HingeJoint * joint;
+    joint = dynamic_cast<HingeJoint*>(world->createJoint(jointInfo)); 
+    return joint;
+}
+
+FixedJoint * create_fixed_joint(
+    PhysicsWorld* world,
+    rabbit::vec3f point,
+    RigidBody* body1,
+    RigidBody* body2)
+{
+    const Vector3 anchorPoint = rabbit::react_cast(point);
+    FixedJointInfo jointInfo(body1, body2, anchorPoint);
+    FixedJoint * joint;
+    joint = dynamic_cast<FixedJoint*>(world->createJoint(jointInfo)); 
+    return joint;
+}
+
 class Manipulator 
 {
+public:
     PhysicsWorld* world;
-    RigidBody* body0;
-    RigidBody* body1;
+    RigidBodyPair base_body;
+    RigidBodyPair base_body_proxy;
+    RigidBodyPair arm1;
+    RigidBodyPair arm2;
 
-    Manipulator(PhysicsWorld* world) 
+    HingeJoint* joint0;
+    HingeJoint* joint1;
+
+    std::vector<RigidBodyPair*> bodies = {};
+    std::vector<HingeJoint*> joints = {};
+
+public:
+    Manipulator(PhysicsWorld* world, const ralgo::pose3<float>& initial_pose = ralgo::pose3<float>())
         : world(world)
-    {}
+    {
+        base_body = RigidBodyPair::make(world, initial_pose * ralgo::mov3<float>({0,0,0}));
+        base_body_proxy = RigidBodyPair::make(world, initial_pose * ralgo::mov3<float>({0,0,0}));
+        arm1 = RigidBodyPair::make(world, initial_pose * ralgo::mov3<float>({0,0,-1}));
+        arm2 = RigidBodyPair::make(world, initial_pose * ralgo::mov3<float>({0,0,-2}));
+        base_body.body->setType(BodyType::STATIC);
+
+        create_fixed_joint(world, base_body.get_pose().lin, base_body_proxy.body, base_body.body);
+        joint0 = create_hinge_joint(world, base_body_proxy.get_pose().lin, {0,1,0}, arm1.body, base_body_proxy.body);
+        joint1 = create_hinge_joint(world, arm1.get_pose().lin, {0,1,0}, arm2.body, arm1.body);
+
+        bodies = {&base_body, &base_body_proxy, &arm1, &arm2};
+        joints = {joint0, joint1};
+    }
+
+    void bind_to_scene(rabbit::scene& scene)
+    {
+        for (auto& body : bodies)
+        {
+            scene.add_object(body->drawer.get());
+        }
+    }
 };
 
 int main() 
@@ -20,47 +139,39 @@ int main()
     rabbit::scene scene;
     auto window = rabbit::create_glfw_window();
     auto view = scene.create_view();
+    view->enable_trihedron();
 
     PhysicsCommon physicsCommon;
 	PhysicsWorld* world = physicsCommon.createPhysicsWorld();
+    world->setGravity(Vector3(0.0, 0.0, -1));
 
-    // New position and orientation of the collision body
-    Vector3 position (0.0 , 0.0 , 0.0) ;
-    Quaternion orientation = Quaternion :: identity () ;
-    Transform newTransform ( position , orientation ) ;
-    RigidBody* body = world->createRigidBody(newTransform);
+    Manipulator manipulator(world);
+    manipulator.bind_to_scene(scene);
 
-    world->setGravity(Vector3(0.0, 0.0, 0.0));
-
-    rabbit::polyline_drawable_object polyline({
-        {0.0, 0.0, 0.0},
-        {1.0, 0.0, 0.0},
-        {0.0, 1.0, 0.0},
-        {0.0, 0.0, 1.0},
-        {0.0, 0.0, 0.0}
-    });
-
-    //view->camera.set_eye(rabbit::vec3f(100.0, 100.0, 100.0));
-    //view->camera.set_target({0, 0, 0});
-
-    scene.add_object(&polyline);
+    HingeJoint * j = manipulator.joint0;
+    j->enableMotor(true);
 
     while (!glfwWindowShouldClose(window)) 
     {
-        view->camera.set_camera(
+        /*view->camera.set_camera(
             rabbit::vec3f{10.f*cos(glfwGetTime()), 10.f*sin(glfwGetTime()), 3},
             {0, 0, 0}
+        );*/
+        view->camera.set_camera(
+            rabbit::vec3f{0.f, 10.f, 3},
+            {0, 0, 0}
         );
-        
-        world->update(1.0f / 60.0f);
+
+    j->setMaxMotorTorque(1);
+    j->setMotorSpeed(1000000);
+
+
+        double timeStep = 1.0f / 60.0f;
+
+        world->update(timeStep);
         scene.update();
 
-        // Get the new position and orientation of the collision body
-        Transform transform = body->getTransform();
-
-        // Print the new position and orientation of the collision body
-        printf("Position: %f %f %f\n", transform.getPosition().x, transform.getPosition().y, transform.getPosition().z);
-    
+        nos::println(rabbit::react_cast(manipulator.joint0->getReactionTorque(timeStep)));
 
         glfwPollEvents();
         glfwSwapBuffers(window);
