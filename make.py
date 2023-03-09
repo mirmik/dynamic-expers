9faf12218a73a9b05daa1677d82fee3192b42e64
mirmik <mirmikns@yandex.ru>
2023-03-09 13:43:50 +0300

    'a'

--- make.py
+++ make.py
@@ -7,7 +7,7 @@ licant.include("ralgo")
 
 licant.cxx_application("app",
                        sources=["src/main.cpp"],
-                       libs=["reactphysics3d", "igris", "nos", "crow"],
+                       libs=["reactphysics3d", "igris", "nos", "crow", "pthread"],
                        mdepends=["rabbit", "ralgo"]
                        )
 
